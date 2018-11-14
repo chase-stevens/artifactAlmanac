5f3e3fa9165bca530d7aab39708aed8965c49f35
chase-stevens <chasestevenspersonal@gmail.com>
2018-11-14 16:54:54 -0700

    added card images

--- src/templates/card.js
+++ src/templates/card.js
@@ -6,7 +6,7 @@ export default ({ pageContext: { card } }) => (
   <div className="post-container">
     <div className="post">
       <h1>{card.card_name.english}</h1>
-      <p>Type: {card.card_type}</p>
+      <img src={card.large_image.default} /><br />
       <Link to="/">
         Home Page
       </Link>
